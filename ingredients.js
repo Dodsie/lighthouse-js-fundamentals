e0f25bfb8a91f699cb0a904b5c2400b025be3e92
Dodsie <99204819+Dodsie@users.noreply.github.com>
2022-04-07 10:38:07 -0600

    Update ingredients.js

--- ingredients.js
+++ ingredients.js
@@ -1,11 +1,10 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
 
+// Write a while loop that prints out the contents of ingredients:
 i = 0
 while (i < ingredients.length) {
   console.log(ingredients[i]);
   i++;}
-
-// Write a while loop that prints out the contents of ingredients:
 
 // Write a for loop that prints out the contents of ingredients:
 for (i = 0; i < ingredients.length; i++) {
